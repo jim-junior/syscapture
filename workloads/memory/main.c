f97b5b88f7012d6f38c3f9de6acc0729eaebe435
Beingana Jim Junior <jimjunior854@gmail.com>
2026-08-17 00:56:46 +0300

    feat: update memory allocation workload to accept timeout parameter
    Signed-off-by: Beingana Jim Junior <jimjunior854@gmail.com>

--- workloads/memory/main.c
+++ workloads/memory/main.c
@@ -8,13 +8,14 @@
 int main(int argc, char *argv[])
 {
 
-  if (argc < 2)
+  if (argc < 3)
   {
-    printf("Usage: %s <size_in_mb>\n", argv[0]);
+    printf("Usage: %s <size_in_mb> <timeout>", argv[0]);
     return 1;
   }
 
   int size_in_mb = atoi(argv[1]);
+  int timeout = atoi(argv[2]);
   size_t size_in_bytes = size_in_mb * MEGABYTE;
 
   // Allocate memory
@@ -28,9 +29,9 @@ int main(int argc, char *argv[])
   // Fill the allocated memory with some data
   memset(buffer, 0, size_in_bytes);
 
-  // Keep the program running for 60 seconds to allow observation of memory usage
-  printf("Allocated %d MB of memory. Sleeping for 60 seconds...\n", size_in_mb);
-  sleep(60);
+  // Keep the program running for the specified timeout to allow observation of memory usage
+  printf("Allocated %d MB of memory. Sleeping for %d seconds...\n", size_in_mb, timeout);
+  sleep(timeout);
 
   // Free the allocated memory
   printf("Freeing allocated memory...\n");
