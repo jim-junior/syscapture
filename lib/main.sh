790c679deb91ab95903ed6ea346c2f8db494ff7a
Beingana Jim Junior <jimjunior854@gmail.com>
2026-09-18 23:25:52 +0300

    feat: update script directory resolution for improved path handling
    Signed-off-by: Beingana Jim Junior <jimjunior854@gmail.com>

--- lib/main.sh
+++ lib/main.sh
@@ -2,6 +2,6 @@
 
 set -euo pipefail
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}")")" && pwd)"
 
 source "$SCRIPT_DIR/collector/host.sh"
--- main.sh
+++ main.sh
@@ -2,7 +2,10 @@
 
 set -euo pipefail
 
-source ./lib/main.sh
+SCRIPT_DIR="$(cd $(dirname $(readlink -f -n "${BASH_SOURCE[0]}")) && pwd)"
+
+source "${SCRIPT_DIR}/lib/main.sh"
+
 
 
 function argument_error() {
